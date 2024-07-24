f22e8afbeafd8ff22c6210dfc96cf5620e9f15be
manisha <aglawe.manisha@gmail.com>
2024-07-24 09:22:58 +0530

    HoverOver

---
+++ Flipkart.java
@@ -0,0 +1,23 @@
+package HoverOver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Flipkart {
+
+	public static void main(String[] args)
+	{
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+		WebElement login=driver.findElement(By.xpath("//a[.='Login']"));
+		Actions a1=new Actions(driver);
+		a1.moveToElement(login).perform();
+		WebElement myprofile=driver.findElement(By.xpath("(//li)[1]"));
+	     myprofile.click();
+
+	}
+
+}
